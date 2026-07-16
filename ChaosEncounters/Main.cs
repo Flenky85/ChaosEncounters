61638b2b487dc650529b5337b345e7fbec220e6d
Flenky <mangaralc@gmail.com>
2026-07-16 09:37:50 +0200

    Create initial Rogue Trader mod project

---
+++ ChaosEncounters/Main.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using System.Text;
+using System.Reflection;
+using UnityModManagerNet;
+
+namespace ChaosEncounters;
+
+public static class Main {
+    internal static Harmony HarmonyInstance;
+    internal static UnityModManager.ModEntry.ModLogger Log;
+
+    public static bool Load(UnityModManager.ModEntry modEntry) {
+        Log = modEntry.Logger;
+        modEntry.OnGUI = OnGUI;
+        HarmonyInstance = new Harmony(modEntry.Info.Id);
+        try {
+            HarmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+        } catch {
+            HarmonyInstance.UnpatchAll(HarmonyInstance.Id);
+            throw;
+        }
+        return true;
+    }
+
+    public static void OnGUI(UnityModManager.ModEntry modEntry) {
+
+    }
+}
